7aaf30b5e8c26ee2652c4cb4049adefe761fa12e
Dmitry Antontsev <antontsev.dmitry@yandex.ru>
2025-04-17 00:24:55 +0300

    [PANIC] Add MAC and status in Host and add host getters and setters

--- include/Core/host.hpp
+++ include/Core/host.hpp
@@ -2,45 +2,38 @@
 #pragma once
 
 #include "Core/IP.hpp"
-
-/*
-      "devices": [
-    {
-      "hostname": "device-1",
-      "ip_address": "192.168.1.10",
-      "mac_address": "00:14:22:01:23:45",
-      "status": "online",
-      "ping": {
-        "status": "success",
-        "response_time_ms": 25
-      },
-      "ports": [
-        {
-          "port": 80,
-          "status": "open"
-        },
-        {
-          "port": 22,
-          "status": "closed"
-        }
-*/
+#include "Core/MAC.hpp"
 
 namespace panic {
+
+enum HostStatus {
+  OFFLINE,
+  ONLINE
+};
 
 class Host
 {
 private:
     std::string hostName_;  // Host name
     IPv4 hostIP_;           // IP address of host
-    // TODO mac address of host
-    // TODO host status: online, offline
+    MAC_48 hostMAC_;        // MAC-48 address of host
+    HostStatus status_;     // Status of host (offline/online)
     // TODO ping status (last response time, response time history)
     // TODO ports list  { "port": 22, "status": "closed"}
 public:
-    Host() : hostIP_(0), hostName_("noname") {};
-    explicit Host(IPv4 IP) : hostIP_(IP), hostName_("noname") {};
+    Host() : hostIP_(0), hostName_("noname"), hostMAC_(0), status_(OFFLINE) {};
 
+    // Getters
     std::string get_name() const {return hostName_;}
+    IPv4 get_IPv4() const {return hostIP_;}
+    MAC_48 get_MAC() const {return hostMAC_;}
+    HostStatus getStatus() const {return status_;}
+
+    // Setters
+    void set_name(const std::string &hostName) {hostName_ = hostName;}
+    void set_IPv4(const IPv4 &IP) {hostIP_ = IP;}
+    void set_MAC(const MAC_48 &MAC) {hostMAC_ = MAC;}
+    void set_status(HostStatus status) {status_ = status;}
 };
 
 } // namespace panic
